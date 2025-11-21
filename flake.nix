d23354ae1c50e28251293bae37d2cce4f7e1f5ef
NullBite <me@nullbite.com>
2025-11-21 02:04:24 -0500

    flake: set nixpkgs input url

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "dotfiles-ng";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
